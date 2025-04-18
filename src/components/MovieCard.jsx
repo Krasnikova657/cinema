a2d2ebdcc8d5d9bfbf3442d1f85320c5dfe9737a
Valeriia bc2024-6 <Valeriia.Krasnikova@lnu.adu.ua>
2025-04-19 00:38:14 +0300

    Додано зміни постерів

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import '../styles/MovieCard.css';
 
 const MovieCard = ({ movie }) => (
   <div className="movie-card">
-    <img src={movie.image} alt={movie.title} />
+    <img src={movie.poster} alt={movie.title} />
     <h3>{movie.title}</h3>
     <p>{movie.description}</p>
     <span>Жанр: {movie.genre}</span>
--- src/data/movies.js
+++ src/data/movies.js
@@ -1,21 +1,132 @@
-
 const movies = [
-  { id: 1, title: "Інтерстеллар", description: "Космічна подорож для порятунку людства.", genre: "Фантастика", time: "2025-04-25 18:00", image: "https://placehold.co/300x400?text=Interstellar" },
-  { id: 2, title: "Темний лицар", description: "Бетмен протистоїть Джокеру в Ґотемі.", genre: "Бойовик", time: "2025-04-25 20:00", image: "https://placehold.co/300x400?text=Dark+Knight" },
-  { id: 3, title: "Початок", description: "Проникнення у сни заради місії.", genre: "Трилер", time: "2025-04-26 19:00", image: "https://placehold.co/300x400?text=Inception" },
-  { id: 4, title: "Аватар", description: "Пандора: дивовижна планета та її жителі.", genre: "Фентезі", time: "2025-04-26 16:00", image: "https://placehold.co/300x400?text=Avatar" },
-  { id: 5, title: "Месники", description: "Герої Землі об'єднуються проти загрози.", genre: "Супергеройський", time: "2025-04-27 21:00", image: "https://placehold.co/300x400?text=Avengers" },
-  { id: 6, title: "Титанік", description: "Трагічна історія кохання на борту лайнера.", genre: "Романтика", time: "2025-04-28 17:30", image: "https://placehold.co/300x400?text=Titanic" },
-  { id: 7, title: "Джокер", description: "Історія становлення культового лиходія.", genre: "Драма", time: "2025-04-28 19:30", image: "https://placehold.co/300x400?text=Joker" },
-  { id: 8, title: "Матриця", description: "Світ ілюзій, у якому панує штучний інтелект.", genre: "Фантастика", time: "2025-04-29 18:00", image: "https://placehold.co/300x400?text=Matrix" },
-  { id: 9, title: "Людина-павук", description: "Початок супергероя з Нью-Йорка.", genre: "Супергеройський", time: "2025-04-29 20:00", image: "https://placehold.co/300x400?text=Spider-Man" },
-  { id: 10, title: "Форрест Ґамп", description: "Життя простого чоловіка з великим серцем.", genre: "Драма", time: "2025-04-30 18:00", image: "https://placehold.co/300x400?text=Gump" },
-  { id: 11, title: "Гладіатор", description: "Генерал стає рабом і героєм арени.", genre: "Історичний", time: "2025-04-30 20:00", image: "https://placehold.co/300x400?text=Gladiator" },
-  { id: 12, title: "Топ Ґан", description: "Життя та ризики льотчика-аса.", genre: "Бойовик", time: "2025-05-01 19:00", image: "https://placehold.co/300x400?text=Top+Gun" },
-  { id: 13, title: "Ла-Ла Ленд", description: "Мрії, музика і кохання в Лос-Анджелесі.", genre: "Мюзикл", time: "2025-05-02 17:00", image: "https://placehold.co/300x400?text=La+La+Land" },
-  { id: 14, title: "Дюна", description: "Боротьба за спецію на пустельній планеті.", genre: "Фантастика", time: "2025-05-02 19:30", image: "https://placehold.co/300x400?text=Dune" },
-  { id: 15, title: "Чорна пантера", description: "Новий король Ваканди та його виклики.", genre: "Супергеройський", time: "2025-05-03 20:00", image: "https://placehold.co/300x400?text=Black+Panther" },
-  { id: 16, title: "Шлях додому", description: "Собака долає кілометри, щоб повернутися до хазяїна.", genre: "Сімейний", time: "2025-05-04 17:00", image: "https://placehold.co/300x400?text=A+Dog's+Way+Home" }
+  {
+    id: 1,
+    title: "Втеча з Шоушенка",
+    description: "Невинно засуджений чоловік планує втечу з в'язниці.",
+    genre: "Драма",
+    time: "19:00",
+    poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+  },
+  {
+    id: 2,
+    title: "Форрест Гамп",
+    description: "Історія життя простого чоловіка з великим серцем.",
+    genre: "Драма",
+    time: "20:00",
+    poster: "https://image.tmdb.org/t/p/w500/saHP97rTPS5eLmrLQEcANmKrsFl.jpg"
+  },
+  {
+    id: 3,
+    title: "Темний лицар",
+    description: "Бетмен протистоїть Джокеру, який тероризує Готем.",
+    genre: "Екшн",
+    time: "21:00",
+    poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+  },
+  {
+    id: 4,
+    title: "Початок",
+    description: "Група професіоналів виконує складну місію у світі снів.",
+    genre: "Фантастика",
+    time: "22:00",
+    poster: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
+  },
+  {
+    id: 5,
+    title: "Матриця",
+    description: "Програміст дізнається правду про реальність.",
+    genre: "Фантастика",
+    time: "18:00",
+    poster: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg"
+  },
+  {
+    id: 6,
+    title: "Кримінальне чтиво",
+    description: "Переплетення історій злочинців у Лос-Анджелесі.",
+    genre: "Кримінал",
+    time: "20:30",
+    poster: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+  },
+  {
+    id: 7,
+    title: "Гладіатор",
+    description: "Римський генерал стає гладіатором, щоб помститися.",
+    genre: "Історичний",
+    time: "19:30",
+    poster: "https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg"
+  },
+  {
+    id: 8,
+    title: "Титанік",
+    description: "Трагічна історія кохання на борту «Титаніка».",
+    genre: "Драма",
+    time: "21:00",
+    poster: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"
+  },
+  {
+    id: 9,
+    title: "Зелена миля",
+    description: "Охоронець в'язниці знайомиться з незвичайним ув'язненим.",
+    genre: "Драма",
+    time: "20:00",
+    poster: "https://image.tmdb.org/t/p/w500/velWPhVMQeQKcxggNEU8YmIo52R.jpg"
+  },
+  {
+    id: 10,
+    title: "Володар перснів: Братство персня",
+    description: "Група героїв вирушає знищити могутній перстень.",
+    genre: "Фентезі",
+    time: "18:30",
+    poster: "https://image.tmdb.org/t/p/w500/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg"
+  },
+  {
+    id: 11,
+    title: "Зоряні війни: Нова надія",
+    description: "Молодий фермер приєднується до повстанців проти Імперії.",
+    genre: "Фантастика",
+    time: "19:00",
+    poster: "https://image.tmdb.org/t/p/w500/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg"
+  },
+  {
+    id: 12,
+    title: "Пірати Карибського моря: Прокляття Чорної перлини",
+    description: "Капітан Джек Спарроу шукає скарб і стикається з проклятими піратами.",
+    genre: "Пригоди",
+    time: "20:00",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTguKIbJ6rhSiru0qNv9B0zhB3NGvAEORweGg&s"
+  },
+  {
+    id: 13,
+    title: "Інтерстеллар",
+    description: "Група астронавтів шукає новий дім для людства.",
+    genre: "Фантастика",
+    time: "21:30",
+    poster: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
+  },
+  {
+    id: 14,
+    title: "Ла-Ла Ленд",
+    description: "Історія музиканта і акторки, що шукають успіх у Лос-Анджелесі.",
+    genre: "Мюзикл",
+    time: "18:45",
+    poster: "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg"
+  },
+  {
+    id: 15,
+    title: "Джокер",
+    description: "Поступове перетворення звичайної людини на Джокера.",
+    genre: "Психологічна драма",
+    time: "22:00",
+    poster: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
+  },
+  {
+    id: 16,
+    title: "Чорна пантера",
+    description: "Король Ваканди бере на себе відповідальність за свій народ.",
+    genre: "Супергеройський",
+    time: "20:15",
+    poster: "https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg"
+  }
 ];
 
 export default movies;
