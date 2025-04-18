5f5f25700ea29a5bb3997b6eb980cfd527624818
Valeriia bc2024-6 <Valeriia.Krasnikova@lnu.adu.ua>
2025-04-18 17:56:03 +0300

    Додано компоненти та заповнено файл movies

---
+++ src/data/moves.js
@@ -0,0 +1,20 @@
+const movies = [
+    {
+      id: 1,
+      title: "Інтерстеллар",
+      description: "Науково-фантастична подорож через простір і час.",
+      genre: "Фантастика",
+      time: "2025-04-25 18:00",
+      image: "https://link-to-poster1.jpg"
+    },
+    {
+      id: 2,
+      title: "Темний лицар",
+      description: "Бетмен проти Джокера.",
+      genre: "Екшн",
+      time: "2025-04-25 20:00",
+      image: "https://link-to-poster2.jpg"
+    }
+  ];
+  export default movies;
+  
